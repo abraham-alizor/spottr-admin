429fcbe509306f1cf12a9a7bb784d4bd061a2e20
Vickyvackydev <vickyvacky5@gmail.com>
2024-05-23 00:17:49 +0100

    feat: create task

--- src/services/tasks/task.service.tsx
+++ src/services/tasks/task.service.tsx
@@ -6,3 +6,8 @@ export const TaskApi = async () => {
   const response: TaskApiResponse = await API.get("admin/tasks");
   return response?.data?.data;
 };
+
+export const CreateTaskApi = async (data: any) => {
+  const response: TaskApiResponse = await API.post("/tasks", data);
+  return response?.data;
+};
